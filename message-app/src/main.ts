ae6e04ae3405154f7804fa3011b7117fab3152a8
gitgitWi <johnwi@knou.ac.kr>
2021-07-26 22:09:12 +0900

    Feat: Swagger 추가

--- message-app/src/main.ts
+++ message-app/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(MessagesModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(`Simple JSON Message App API`)
+    .setVersion(`1.0`)
+    .build();
+
+  const swaggerDocs = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/swagger', app, swaggerDocs);
+
   await app.listen(3000);
-}
+};
 
 bootstrap();
--- message-app/src/messages/messages.controller.ts
+++ message-app/src/messages/messages.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('messages')
 @Controller('messages')
 export class MessagesController {
   @Get()
