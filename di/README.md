60b63dfbf9cc24881636c4666f621fdfbadc2ef6
gitgitWi <johnwi@knou.ac.kr>
2021-07-30 17:52:37 +0900

    Feat: DI, Mutli-Module 연습

--- di/README.md
+++ di/README.md
@@ -1,3 +1,26 @@
 # 03. DI 연습 프로젝트
 
-Nest DI에 익숙해지기 위한 짧은 프로젝트
+Nest DI, Multi Module에 익숙해지기 위한 짧은 프로젝트
+
+---
+
+Module 내부의 `Service`, `Repository` 등 의존성은 모두 private
+
+다른 Module에서 사용하기 위해선 `exports`에 등록해야 함
+
+```typescript
+@Module({
+  imports: [PowerModule],
+  providers: [DiskService],
+  exports: [DiskService],
+})
+export class DiskModule {}
+
+// ,,,
+
+@Module({
+  imports: [CpuModule, DiskModule],
+  controllers: [ComputerController],
+})
+export class ComputerModule {}
+```
--- di/src/main.ts
+++ di/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NextFunction, Request } from 'express';
+import { ComputerModule } from './computer/computer.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(ComputerModule);
+
+  app.use((req: Request, _, next: NextFunction) => {
+    console.log(
+      `[CustomLogger] ${new Date().toLocaleString()}\tRequested URL: ${
+        req.url
+      }`,
+    );
+    next();
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
